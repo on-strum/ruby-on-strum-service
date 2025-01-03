edad0f5aacc2540913a819d2fc88ed8073a2184b
Vladislav Trotsenko <admin@bestweb.com.ua>
2025-01-03 17:55:53 +0100

    Bugfix/Compatibility issues with Strum::Service

--- lib/on_strum/service.rb
+++ lib/on_strum/service.rb
@@ -6,10 +6,10 @@ module OnStrum
   module Service
     def self.included(base)
       base.extend(OnStrum::Service::ClassMethods)
-      base.prepend(OnStrum::Service::InstanceMethods)
-    end
+      base.include(OnStrum::Service::InstanceMethods)
 
-    def audit; end
+      ::Kernel.warn(OnStrum::Service::WARNING) if defined?(Strum::Service)
+    end
 
     def call
       raise OnStrum::Service::Error::Runtime, OnStrum::Service::Error::Runtime::NOT_IMPLEMENTED
